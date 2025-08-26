fd81ec2e8146703918c36250cdeeaea5da84641b
wangyutang198 <wangyutang198@gmail.com>
2025-08-26 21:10:29 +0800

    computed計算屬性

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,18 @@
 <template>
-  <p v-for="text in dataList">{{ text }}</p>
+  <h1>Computed計算屬性</h1>
+
+  <h3>{{ numFormat }}</h3>
 </template>
 
 <script setup>
-const dataList = ["a", "b", "c", "d"];
+import { computed } from "vue";
+const num = 0.5;
+
+const numFormat = computed(() => {
+  if (Number.isNaN(num * 100)) return num;
+  return `${num * 100}%`;
+});
 </script>
 
 <style scoped></style>
+
